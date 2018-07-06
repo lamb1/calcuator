c85de5f08ac0939362c29650e88c271ace7ced75
xuxiaoshan <xuxiaoshan@lvshou.com>
2018-07-06 16:22:03 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,13 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
 import Index from '@/pages/Index';
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
-    },
-    {
-      path: '/Index',
-      name: 'Index',
-      component: Index,
-    },
-  ],
+    routes: [{
+        path: '/',
+        name: 'Index',
+        component: Index,
+    }, ],
 });
